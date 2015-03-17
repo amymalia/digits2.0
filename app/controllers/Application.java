916e52fa95e81eb0827d53368a19c71c95fdabee
amymalia <amytakayesu@gmail.com>
2015-03-17 02:35:16 -1000

    added form functionality
    Added form functionality

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,8 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Index;
 import views.html.NewContact;
+import views.formdata.ContactFormData;
+import play.data.Form;
 
 /**
  * Provides controllers for this application.
@@ -23,8 +25,20 @@ public class Application extends Controller {
    * @return The NewContact.
    */
   public static Result newContact() {
-    return ok(NewContact.render("Welcome to NewContact."));
+    Form<ContactFormData> formData = Form.form(ContactFormData.class);
+    return ok(NewContact.render(formData));
 
+  }
+
+  /**
+   * Processes form submitted from newContact page
+   * @return Result of the New Contact page
+   */
+  public static Result postContact() {
+    Form<ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
+    ContactFormData data = formData.get();
+    System.out.println("OK: " + data.firstName + " " + data.lastName + " " + data.telephone);
+    return ok(NewContact.render(formData));
   }
 
 }
---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,11 @@
+package views.formdata;
+
+/**
+ * Created by amytakayesu on 3/17/15.
+ */
+public class ContactFormData {
+  public String firstName = "";
+  public String lastName = "";
+  public String telephone = "";
+
+}
